c1f55ecca12247975a31a7f9cf6e7bc4c6d0b8cd
kevinkleiman <kxkleiman@gmail.com>
2020-02-26 11:41:17 -0700

    Threading functions added

--- secp2p.py
+++ secp2p.py
@@ -1,33 +1,50 @@
 import socket
 import sys
+import threading
 
 host = sys.argv[1] 
 port = 8000     
 
-
 def server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('localhost', port))
+        name = socket.gethostname()
+        ip = socket.gethostbyname(name)
+        s.bind((ip, port))
         s.listen()
-        print('[*] Waiting for Connection'.format(port))
+        print('[*] Waiting for Connection on {}'.format(ip))
         conn, addr = s.accept()
         with conn:
+            conn.sendall(b'[*] Connected to {} on port {}'.format(ip, port))
             while True:
-                connected = '[*] Connected to {}'.format(addr)
-                conn.sendall(connected.encode())
                 data = conn.recv(1024)
-                if not data:
+                print('[REMOTE] ' + data.decode())
+                if data == '!close':
+                    s.close()
+                    print('[-] Connection closed by remote')
                     break
                 conn.sendall(data)
 
 def client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        msg = input()
-        print('# ')
-        s.sendall(msg.encode())
+        while True:
+            try:
+                s.connect((host, port))
+                break
+            except:
+                pass
+
+        while True:
+            msg = input('# ')
+            if msg == '!close':
+                s.sendall(b'!close')
+                s.close()
+                break
+            print('[YOU] ' + msg)
+            s.sendall(msg.encode())
 
 
 if __name__ == '__main__':
-    server()
-    client()
+    tserver = threading.Thread(name='server', target=server)
+    tclient = threading.Thread(name='client', target=client)
+    tserver.start()
+    tclient.start()
